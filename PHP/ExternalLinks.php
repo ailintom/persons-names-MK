cbb00ef7e0a7f09993173f2c9b3a59b314ef6b69
Alexander Ilin-Tomich <ilintomich@gmail.com>
2023-09-28 08:25:18 +0200

    update TLA links and reset button style

--- PHP/ExternalLinks.php
+++ PHP/ExternalLinks.php
@@ -15,7 +15,8 @@ class ExternalLinks
     const TRISMEGISTOS_TEXTS = 'https://www.trismegistos.org/text/';
     const TLA_TEXTS = 'https://aaew.bbaw.de/tla/servlet/GetTextDetails?tc=';
     const THOTH_CONCEPT = 'http://thot.philo.ulg.ac.be/concept/';
-    const TLA = 'http://aaew.bbaw.de/tla/servlet/GetWcnDetails?wn=';
+    // const TLA = 'http://aaew.bbaw.de/tla/servlet/GetWcnDetails?wn=';
+    const TLA = 'https://thesaurus-linguae-aegyptiae.de/lemma/';
     const AGEA = 'http://www.ifao.egnet.net/bases/agea/noms/?id=';
     const TOP_BIB = 'http://topbib.griffith.ox.ac.uk//dtb.html?topbib=';
     const OEB = 'http://oeb.griffith.ox.ac.uk/oeb_entry.aspx?item=';
